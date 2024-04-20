3980eef6dcdca43a108968c3d2558306ed327efb
DavidG <garxia0710@gmail.com>
2024-04-20 15:11:08 -0500

    upToDate

--- item.js
+++ item.js
@@ -12,11 +12,26 @@ const render = async () => {
   refer.src = producto.image;
   refer.alt = producto.name;
 
-  const descripción = document.querySelector(".subtext");
+  const titulo = document.querySelector(".subtext");
+  titulo.textContent = producto.name;
+
+  const descripción = document.querySelector(".item_description");
   descripción.textContent = producto.description;
+
 
   const precio = document.querySelector(".item_price");
     precio.textContent = `$${producto.price}`;
+
+  const contadorProductos = document.querySelector("#contador_productos");
+  const car = document.querySelector(".carrito");
+  let contador = 0;
+  car.addEventListener("click",function () {
+     contador++;
+     contadorProductos.textContent = `Productos agregados al carrito: ${contador}`;
+    
+  });
+  
+  
 }
 
 document.addEventListener('DOMContentLoaded', render);
